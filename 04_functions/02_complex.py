93f18ad58ce5ff37060dafb85d1718f6fabd1863
akshjuneja13 <akshjuneja13@gmail.com>
2026-04-08 13:49:02 -0400

    uptd 4/8

---
+++ 04_functions/02_complex.py
@@ -0,0 +1,24 @@
+# You are creating a monthly report for cafe sales
+
+# Task:
+# Write a function generate_report() that calls:
+# - fetch_sales()
+# - filter_valid_orders()
+# - summarize_data()
+
+def fetch_sales():
+    print("Fetching sales data...")
+
+def filter_valid_orders():
+    print("Filtering valid orders...")
+
+def summarize_data():
+    print("Summarizing data...")
+
+def generate_report():
+    fetch_sales()
+    filter_valid_orders()
+    summarize_data()
+
+# Run the report
+generate_report()
