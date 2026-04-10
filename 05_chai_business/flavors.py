7368757c56d837a00dfb091502cfc65aa19bcdc4
akshjuneja13 <akshjuneja13@gmail.com>
2026-04-10 12:36:55 -0400

    4/10

---
+++ 05_chai_business/flavors.py
@@ -0,0 +1,8 @@
+def elaichi_chai():
+    """Return Elaichi (cardamom) chai description."""
+    return "Elaichi Chai"
+
+
+def ginger_chai():
+    """Return Ginger chai description."""
+    return "Ginger Chai"
---
+++ 05_chai_business/main.py
@@ -0,0 +1,8 @@
+import receipes.flavours
+
+print(receipes.flavours.elaichi_chai())
+
+
+from receipes.flavours import ginger_chai
+
+print(ginger_chai())
